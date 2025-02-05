8548a0c74c5c2b03f2fa0293495554945db64879
kris@kriss <krizclark@gmail.com>
2025-02-05 13:05:15 +0000

    Graphical strings created, needs splitting

--- graphical.py
+++ graphical.py
@@ -1,31 +1,52 @@
 from logic import *
 
 class Card:
-    def __init__(self,card_number,card_suit):
-        self = self
-        self.card_number = card_number
-        self.card_suit = card_suit
+    def __init__(self,hand):
+        self.hand = hand
+        self.face = ""
+        self.n = [i[1] for i in self.hand] #Numbers are seperated into number only list
+        self.s = [i[0] for i in self.hand] #Symbols  "   "     "   ""  symbol "  " "  "
+        n = self.n
+        s = self.s
+        N = ' '.join(str(e) for e in n) #List in joined and still n
+        S = ' '.join(str(e) for e in s) # ""  "" "    "
+        card_quantity = len(self.hand)
+        Card.get_print(self,N,S,card_quantity)
+    
+    def get_print(self,N,S,card_quantity): 
+        x = -2
+        i = card_quantity
+        self.print = ""
+        while i != 0:
+            x += 2
+            a = S[x]
+            b = N[x]
+            self.face = f'''
+                    .---------.
+                    | {b}       |
+                    |         |
+                    |    {a}    |
+                    |         |
+                    |       {b} |
+                    '---------'
+                    '''
+            i -= 1
+            self.print += self.face
+        else:
+            self.illustrate()
 
-    def ilustrate(self,card_number,card_suit):
-        n = card_suit
-        a = card_number
-        if n:
-            if a: 
-                return  f'''\
-                        .-------.
-                        |{a }      |
-                        |       |
-                        |   {n }   |
-                        |       |
-                        |      {a}|
-                        '-------'
-                        '''
-                
+    def illustrate(self):
+        pass
+
+hand = ["♥T","♣4","♣7"]
+Card(hand)
 
 
-
-
-#spacer = ' ' * 5
-#for a, b in zip(a.splitlines(), b.splitlines()):
-#    print(f'{a}{spacer}{b}')
-#print(pack.double())
+class savedprotocol():
+    #spacer = "" * 5
+    #for a, b, c in zip(a.splitlines(), b.splitlines(), c.splitlines()):
+    #   print(f'{a}{spacer}{b}{spacer}{c}')
+    #for d, e, in zip(d.splitlines(),e.splitlines()):
+    #    print(f'{d}{spacer}{e}')
+    #print(pack.double())
+    pass
